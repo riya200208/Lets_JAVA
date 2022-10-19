a3b283e57edd48372e329f6596c5cb9af9d616bd
Vikash Kumar <vikashraj041013@gmail.com>
2022-10-20 01:15:30 +0530

    Added a quiz app

---
+++ Quzz App/app/src/main/java/com/kaushikrana/myapplicationdemo/dashboard.java
@@ -0,0 +1,48 @@
+package com.kaushikrana.myapplicationdemo;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class Dashboard extends AppCompatActivity {
+
+    String EmailHolder;
+    TextView Email;
+    Button LogOUT ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        Email = (TextView)findViewById(R.id.textView1);
+        LogOUT = (Button)findViewById(R.id.button1);
+
+        Intent intent = getIntent();
+
+        // Receiving User Email Send By MainActivity.
+        EmailHolder = intent.getStringExtra(MainActivity.UserEmail);
+
+        // Setting up received email to TextView.
+        Email.setText(Email.getText().toString()+ EmailHolder);
+
+        // Adding click listener to Log Out button.
+        LogOUT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Finishing current DashBoard activity on button click.
+                finish();
+
+                Toast.makeText(Dashboard.this,"Log Out Successful", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+}
